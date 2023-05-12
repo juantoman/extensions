69fb4968b5f3b83cb4e9586b684d64450157dc96
juantoman <juan.torres@iestacio.com>
2023-05-12 14:07:42 +0200

    casi

--- links/popup.js
+++ links/popup.js
@@ -1,14 +1,12 @@
-let n=0
-let k=0
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-if (request.action === "capturar_click") {
-    n++
-    console.log("clicks: " + n);
-    document.getElementById("clicks").textContent = n;
-}
-if (request.action === "capturar_key") {
-    k++
-    console.log("keys: " + k);
-    document.getElementById("keys").textContent = k;
-}
+    if (request.action === "capturar_click") {
+        n++
+        console.log("clicks: " + n);
+        document.getElementById("clicks").textContent = n;
+    }
+    if (request.action === "capturar_key") {
+        k++
+        console.log("keys: " + k);
+        document.getElementById("keys").textContent = k;
+    }
 });
