5238bedb1eada6aedc7b3f0cf5f32722fe128137
juantoman <juan.torres@iestacio.com>
2023-05-12 13:35:27 +0200

    proves

--- links/background.js
+++ links/background.js
@@ -1,10 +1,19 @@
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    chrome.tabs.executeScript(details.tabId, {file: "content.js"});
-});
-
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action == "showLinks") {
-        chrome.tabs.create({url: message.url});
-    }
+//let n=0
+//let k=0
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "capturar_click") {
+    //console.log(request.url);
+    //n++
+    console.log("clicks: ");
+    //document.getElementById("clicks").textContent = n;
+    // Aquí puedes agregar el código para guardar la URL capturada
+  }
+  if (request.action === "capturar_key") {
+    //console.log(request.url);
+    //k++
+    console.log("keys: ");
+    //document.getElementById("clicks").textContent = k;
+    // Aquí puedes agregar el código para guardar la URL capturada
+  }
 });
   
